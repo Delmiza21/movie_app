8249d30d4bf55a856593c7be3672471e7daca2d9
Delmiza21 <delmipongo@gmail.com>
2024-12-03 12:38:15 -0500

    Actualizo homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,8 +47,8 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              Image.asset(
-                "Assets/images/carro.jpg",
+              Image.network(
+                "https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
               ),
               Padding(
                 padding: const EdgeInsets.all(20.0),
